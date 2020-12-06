69d5e754e6a7d2b5ff3dd0d9615e3583c533b674
Szita Tamas <szita999@gmail.com>
2020-12-06 20:05:19 +0100

    first commit

---
+++ kviz.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# konstansok
+kerdesek=kerdesek.txt
+pont1=0
+pont2=0
+
+# van-e kerdes
+if [ ! -f $kerdesek ]
+then
+    echo "Nincsenek kerdesek!"
+    exit 1
+fi
+
+# jatekmenet
+exec 3<&0
+ while IFS=';' read -ra kerdes; do
+      currentQuestion="${kerdes[0]}"
+      answer1="${kerdes[1]}"
+      answer2="${kerdes[2]}"
+      answer3="${kerdes[3]}"
+      rightAnswer="${kerdes[4]}"
+      # kerdesek kiirasa
+          echo " $currentQuestion"
+          echo " a, $answer1"
+          echo " b, $answer2"
+          echo " c, $answer3"
+      # valasz bekerese
+      read -p "Első játékos válasza: " valasz1 <&3
+      read -p "Második játékos válasza: " valasz2 <&3
+     if [ "$valasz1" == "$rightAnswer" ]
+     then
+         pont1=$(( ++pont1 ))
+     fi
+      if [ "$valasz2" == "$rightAnswer" ]
+     then
+         pont2=$(( ++pont2 ))
+     fi
+
+          echo "A jó válasz a '$rightAnswer' volt"
+          echo "A pontok állása:"
+
+ done < "$kerdesek"
+          echo "Első játékos - $pont1"
+          echo "Második játékos - $pont2"
+
+
+ # https://unix.stackexchange.com/questions/453646/bash-prompting-for-user-input-while-reading-file
