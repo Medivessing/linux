94586fa8adf8752ae31335a471f8cb6917765aa4
Szita Tamas <szita999@gmail.com>
2020-12-10 23:35:20 +0100

    final

--- kviz.sh
+++ kviz.sh
@@ -12,6 +12,8 @@ GREEN='\033[032m'
 ORANGE='\033[033m'
 BLUE='\033[034m'
 PURPLE='\033[035m'
+CYAN='\033[036m'
+GRAY='\033[037m'
 NC='\033[0m' # No Color
 
 # van-e kerdes
@@ -30,19 +32,21 @@ exec 3<&0
         answer3="${kerdes[3]}"
         rightAnswer="${kerdes[4]}"
         # kerdesek kiirasa
-            printf "\n"
+            # törli az előtte lévő kiírásokat
+            printf "\033c"
             printf "$i. "
             echo -e "${PURPLE}$currentQuestion${NC}"
-            echo " a, $answer1"
-            echo " b, $answer2"
-            echo " c, $answer3"
+            echo -e "${CYAN} a,${NC} $answer1"
+            echo -e "${CYAN} b,${NC} $answer2"
+            echo -e "${CYAN} c,${NC} $answer3"
             printf "\n"
         # valaszok bekerese
         printf "${GREEN}Első játékos válasza:${NC} "
         read -r valasz1 <&3
+    # újra kérés amíg nem helyes a válasz
     until [ "$valasz1" == "a" ] || [ "$valasz1" == "b" ] || [ "$valasz1" == "c" ]
     do 
-        echo -e "${RED}Adj meg egy helyes betűt!${NC}"
+        echo -e "${RED}Adj meg egy helyes betűt!${CYAN}(a, b, c)${NC}"
         printf "${GREEN}Első játékos válasza:${NC} "
         read -r valasz1 <&3
     done
@@ -63,10 +67,14 @@ exec 3<&0
         pont2=$(( ++pont2 ))
     fi
     printf "\n"
-    echo -e "A jó válasz a(z) ${ORANGE}'$rightAnswer'${NC} volt"
+    echo -e "A jó válasz a(z) '${ORANGE}$rightAnswer${NC}' volt."
+    printf "${CYAN}Nyomj egy gombot a következő kérdéshez!${NC}"
+    # vár egy karakterre a továbblépéshez
+    read -n 1 <&3
     i=$(( ++i ))
 done < "$kerdesek"
 # pontok kiírása
+printf "\n"
 echo "A pontok állása:"
 printf "\n"
 if (($pont1==$pont2))
